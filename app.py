5ddc927565ea4ffaa838e9f4806d56b0abbbc750
Muhammad Talha Sharif <93639673+745talha@users.noreply.github.com>
2023-10-04 15:52:47 +0500

    files uploading
    files uploading  from local device

---
+++ app.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///todo.db"
+db=SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno=db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(200),nullable=False)
+    desc=db.Column(db.String(500),nullable=False)
+    date=db.Column(db.DateTime,default=datetime.utcnow)
+
+
+    def __repr__(self)->str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route('/', methods=['GET','POST'])
+def hello_world():
+
+    if request.method=="POST":
+        title=request.form["title"]
+        desc=request.form["desc"]
+        todo=Todo(title=title,desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+    alltodo=Todo.query.all()
+    return render_template("index.html",alltodo=alltodo)
+
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    if request.method=="POST":
+        title=request.form["title"]
+        desc=request.form["desc"]
+        todo=Todo.query.filter_by(sno=sno).first()
+        todo.title=title
+        todo.desc=desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+
+    todo=Todo.query.filter_by(sno=sno).first()
+    return render_template("update.html",todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    dtodo=Todo.query.filter_by(sno=sno).first()
+    db.session.delete(dtodo)
+    db.session.commit()
+    return redirect("/")
+
+
+if __name__=="__main__":
+    app.run(debug=True)
